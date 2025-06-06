9e8014a7fe25c7dca26ea0f4ebf7413c1f057802
Sozbad <chris.p.kelly00@gmail.com>
2025-06-07 00:29:19 +0100

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, Request
-from gpt_agent_search import handle_search
-
-app = FastAPI()
-
-@app.get("/search")
-async def search_handler(request: Request):
-    product = request.query_params.get("productName")
-    return handle_search(product)
